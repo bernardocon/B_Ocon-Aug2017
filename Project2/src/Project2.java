15b90c4a9e7769523fd5ea9d7561a367a7db9eed
bernardocon <18oconb52@millshighsch.org>
2017-08-22 09:28:49 -0700

    Initial Commit

---
+++ Project2/src/Project2.java
@@ -0,0 +1,16 @@
+//Bernard Ocon
+//My second Java project
+//August 22 2017
+
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		System.out.println("hello -- this is project 2!");
+		System.out.print("hello -- this my project 2!");
+		System.out.print("");
+		
+		
+		
+	}
+}
